e92f802d5461dd5c1d929b4323555ed999f7250b
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-09-25 13:07:17 +0200

    changed the nav logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import {ModeToggle} from "@/components/ThemeToggle";
+import { ModeToggle } from "@/components/ThemeToggle";
 
 const Navbar = () => {
   return (
     <nav className="p-4 justify-between space-x-4 gap-12 items-center  flex">
       <Link href="/">
-          <Image src="/logo.png" width="50" height="50" alt="Ryan Logo" />
+        <Image src="/logo.svg" width="50" height="50" alt="Ryan Logo" />
       </Link>
       <h1 className="text-lg font-black ">DoRiZZle</h1>
       <ModeToggle />
