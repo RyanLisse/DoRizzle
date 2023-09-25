69e64b0676205457211ab1a9e1f9d1ac97516f9d
RyanLisse <57917217+RyanLisse@users.noreply.github.com>
2023-09-25 13:25:41 +0200

    removed migration file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { ModeToggle } from "@/components/ThemeToggle";
 
 const Navbar = () => {
   return (
     <nav className="p-4 justify-between space-x-4 gap-12 items-center  flex">
       <Link href="/">
-        <Image src="/logo.png" width="50" height="50" alt="Ryan Logo" />
+          <h1 className="text-lg font-black ">Ryan</h1>
       </Link>
-      <h1 className="text-lg font-black ">Ryan</h1>
+
       <ModeToggle />
     </nav>
   );
